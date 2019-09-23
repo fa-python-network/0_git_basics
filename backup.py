e74119ef2e50fa44c89d4928d0f09887fde0ab7a
Skorpx <sdn2000@bk.ru>
2019-09-23 13:31:10 +0300

    IfElifElse

--- backup.py
+++ backup.py
@@ -4,3 +4,9 @@ print("Hi, I'm the number" + str(a)) #NewString
 b = 26
 print(str(a)+str(b))
 print(a+b)
+if b > a:
+    print("B is older")
+elif b == a:
+    print("Broos")
+else:
+    print("A is older")
