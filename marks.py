5dcf95de19f564a177ff498a63a98c5539344f5e
bear718 <55141936+bear718@users.noreply.github.com>
2019-09-23 20:21:18 +0300

    Класс Оценки и метод основная оценка

---
+++ marks.py
@@ -0,0 +1,10 @@
+Class Marks:
+	def __init__(self):
+		pass
+		
+	def getGeneralMark(self):
+		print("Сколько баллов вы поставите за основное задание лабы? Введите число: ")
+		a = input()
+		return int(a)
+		
+	
