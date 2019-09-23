35507b25d97098eef70ca14ce258bf310514902e
krasnovaol <mr.february@bk.ru>
2019-09-23 20:27:49 +0300

    Added method grtng in class

--- classGreeting.py
+++ classGreeting.py
@@ -1,3 +1,18 @@
+import datetime
+
 class Greeting:
     def __init__(self):
         pass
+    
+    def grtng(self):
+        a = datetime.datetime.now()
+
+        if (a.hour >= 0 and a.hour < 6):
+            return "Good night"
+        elif (a.hour >= 6 and a.hour < 12):
+            return "Good morning"
+        elif (a.hour >= 12 and a.hour < 18):
+            return "Good day"
+        else:
+            return "Good evening"
+        
