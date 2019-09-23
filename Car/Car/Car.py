41f0fc476cbcadebf63701c3e6a8b0923d9e521d
TheFirstHunter <kajit.konstantin@yandex.ru>
2019-09-23 20:34:01 +0300

    Window

--- Car/Car/Car.py
+++ Car/Car/Car.py
@@ -1,1 +1,19 @@
+import pygame
+pygame.init()
 
+white=(255, 255, 255)
+sc=pygame.display.set_mode((2000, 1000))
+sc.fill(white)
+pygame.display.set_caption('Car')
+
+
+pygame.display.update()
+
+flag=True
+while flag:
+    for i in pygame.event.get():
+        if i.type==pygame.QUIT:
+            flag=False
+
+
+    pygame.time.delay(20)
