be2272443db93831d657b7d64c5f3df8adaf7d02
bear718 <55141936+bear718@users.noreply.github.com>
2019-09-23 20:32:11 +0300

    Добавлены получения доп оценок

--- marks.py
+++ marks.py
@@ -7,4 +7,19 @@ Class Marks:
 		a = input()
 		return int(a)
 		
-	
+	def getAddMark1(self):
+		print("Сколько баллов вы поставите за доп. задание №1? Введите число: ")
+		b = input()
+		return int(b)
+		
+	def getAddMark2(self):
+		print("Сколько баллов вы поставите за доп. задание №2? Введите число: ")
+		c = input()
+		return int(c)
+		
+	def getAddMark1(self):
+		print("Сколько баллов вы поставите за доп. задание №3? Введите число: ")
+		d = input()
+		return int(d)
+    
+    
