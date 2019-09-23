99a9abc4aa76b3cacb22cc13eb5c5f2cc3e5f945
TheFirstHunter <kajit.konstantin@yandex.ru>
2019-09-23 21:26:35 +0300

    1stUsingClass

--- Car/Car/Car.py
+++ Car/Car/Car.py
@@ -1,12 +1,14 @@
 import pygame
 pygame.init()
+from ClassCar import Car
 
 white=(255, 255, 255)
 sc=pygame.display.set_mode((2000, 1000))
 sc.fill(white)
 pygame.display.set_caption('Car')
 
-
+car=Car(surf)
+sc.blit(car.surf_up, car.rect_up)
 pygame.display.update()
 
 flag=True
