ed828b7539abce0518ddee897391ed1dd35af4dd
krasnovaol <mr.february@bk.ru>
2019-09-23 09:58:59 +0300

    Added if-elif-else with wish

--- backup.py
+++ backup.py
@@ -3,3 +3,11 @@
 print ("Good morning, Michail Viktorovich!")
 print ("Are you drinking coffee now? y/n")
 answer = input()
+
+if answer == "y":
+	print("Enjoy your coffee and good day!")
+elif answer == "n":
+	print (It's so good for your health. Good day!)
+else:
+	print ("Your answer should be y or n. Repeat, please")
+	
