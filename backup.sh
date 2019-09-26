b8258dbbe4fd75947016cefc621c38eec0485858
Alina <kl.ali18@ya.ru>
2019-09-26 18:28:56 +0300

    New BU

---
+++ backup.sh
@@ -0,0 +1,11 @@
+from pprint import pprint 
+matrix = [[0.5,   0,   0,   0,   0],
+          [  1, 0.5,   0,   0,   0],
+          [  1,   1, 0.5,   0,   0],
+          [  1,   1,   1, 0.5,   0],
+          [  1,   1,   1,   1, 0.5]]
+
+matrix_t = list(zip(*matrix)) 
+
+pprint(matrix)
+pprint(matrix_t)
