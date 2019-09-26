96d2f46a3540f9e7477740460797e54aec92af1e
Alina <kl.ali18@ya.ru>
2019-09-26 18:52:24 +0300

    Smth1

--- backup.sh
+++ backup.sh
@@ -1,8 +1,8 @@
-from pprint imporrrrrrrt pprint 
+from pprint import pprint 
 matrix = [[0.5,   0,   0,   0,   0],
-          [  1, 0.5,   0,   0,   0],
-          [  1,   1, 0.5,   0,   0],
-          [  1,   1,   1, 0.5,   0],
+          [  1, 0.5,   9,   0,   0],
+          [  1,   1, 0.5,   60,   0],
+          [  1,   10,   1, 0.5,   0],
           [  1,   1,   1,   1, 0.5]]
 
 matrix_t = list(zip(*matrix)) 
