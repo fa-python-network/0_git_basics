8cd83d6c240ba9cafad6f2ac9cc249d30c2ed0e2
Semyon Bochkaryov <squizduos@gmail.com>
2021-12-08 13:59:52 +0300

    Added comment

--- backup.sh
+++ backup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 /usr/bin/rclone copy --update --verbose --transfers 30 --checkers 8 --contimeout 60s --timeout 300s --retries 3 --low-level-retries 10 --stats 1s "/home/sbochkarev/Documents" "gdrive:work-computer"
+
+# sample text
