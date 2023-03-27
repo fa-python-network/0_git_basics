476d97f3c0720527f10e8a95b88c66bf4959a456
Artemiy <artemy.saltsin@gmail.com>
2023-03-27 14:26:28 +0300

    World and auth dump added

--- backup.sh
+++ backup.sh
@@ -1,2 +1,4 @@
 : TODO world and auth backup
-mysqldump -uroot -p characters > dump.sql
+mysqldump -uroot -p characters > characters_dump.sql
+mysqldump -uroot -p world > world_dump.sql
+mysqldump -uroot -p auth > auth_dump.sql
