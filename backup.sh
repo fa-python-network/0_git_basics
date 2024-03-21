d14f5b71ebaa22aff5dc5a7c6cb9b864270325eb
ILUNOSHA~ <dimakorolev2000@gmail.com>
2024-03-22 01:22:19 +0300

    Revert "error"
    This reverts commit 5c9e8f98d7c05f3850130aa63c6e51fa15dc275f.

--- backup.sh
+++ backup.sh
@@ -1,2 +1,2 @@
-ec "Hello, we are students of Financial University PI22-2V, Dmitriy Korolev, Maria Parusnikova, Nikita Voronov, Valeria Markusheva."
+echo "Hello, we are students of Financial University PI22-2V, Dmitriy Korolev, Maria Parusnikova, Nikita Voronov, Valeria Markusheva."
 #comment
